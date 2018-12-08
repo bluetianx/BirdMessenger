970f6ae53ff7efb4dc60b1442512d06fc29b0f39
BruceTian <brucetianx@163.com>
2018-12-08 20:10:53 +0800

    修改测试站点的端口

--- samples/testSite/app.js
+++ samples/testSite/app.js
@@ -8,8 +8,11 @@ server.datastore = new tus.FileStore({
 const app = express();
 const uploadApp = express();
 uploadApp.all('*', server.handle.bind(server));
+app.get('/',function(req,res){
+    res.send('upload site by tian');
+});
 app.use('/uploads', uploadApp);
 
-const host = '127.0.0.1';
+const host = '0.0.0.0';
 const port = 1080;
 app.listen(port, host);
