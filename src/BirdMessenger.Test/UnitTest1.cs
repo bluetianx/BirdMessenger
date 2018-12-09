75e53e8bdf6589c3019fe91de7fbc17fe850f74f
BruceTian <brucetianx@163.com>
2018-12-09 21:36:23 +0800

    UnitTest compareFileby Hash

--- src/BirdMessenger.Test/UnitTest1.cs
+++ src/BirdMessenger.Test/UnitTest1.cs
@@ -1,6 +1,8 @@
 using System;
 using Xunit;
 using BirdMessenger;
+using System.Security.Cryptography;
+
 namespace BirdMessenger.Test
 {
     public class UnitTest1
@@ -9,6 +11,40 @@ namespace BirdMessenger.Test
         public void Test1()
         {
 
+        }
+
+        public static bool CompareFile(string sourceFile,string targetFile)
+        {
+            byte[] sourceData = File.ReadAllBytes(sourceFile);
+            byte[] targetData = File.ReadAllBytes(targetFile);
+            bool resultCompare=false;
+            using(SHA256 sHA256=SHA256.Create())
+            {
+                string hashOfSource = GetHash(sHA256,sourceData);
+                resultCompare= VerifyHash(sHA256,targetData,hashOfSource);
+            }
+            return resultCompare;
+        }
+
+        public static string GetHash(HashAlgorithm hashAlgorithm,byte[] data)
+        {
+            byte[] hashData = hashAlgorithm.ComputeHash(data);
+
+            var sBuilder = new StringBuilder();
+
+            for(int i=0; i<hashData.Length;i++)
+            {
+                sBuilder.Append(hashData[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        public static bool VerifyHash(HashAlgorithm hashAlgorithm,byte[] data, string hash)
+        {
+            string hashOfData = GetHash(hashAlgorithm,data);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            return comparer.Compare(hash,hashOfData) == 0;
         }
     }
 }
