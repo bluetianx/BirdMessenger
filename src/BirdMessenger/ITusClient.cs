43315b5683348136222e2d2c3685989a1515fbb4
BruceTian <brucetianx@163.com>
2020-06-16 22:31:39 +0800

    add interface

--- src/BirdMessenger/ITusClient.cs
+++ src/BirdMessenger/ITusClient.cs
@@ -1,7 +1,12 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace BirdMessenger
 {
     public interface ITusClient
     {
-        
+         Task<bool> Upload(Uri url,FileInfo uploadFileInfo,CancellationToken ct=default(CancellationToken))
     }
 }
