0b913915803006c1cf285c22f14b4bb6a8fe6550
brucetian <brucetianx@gmail.com>
2022-07-27 00:51:54 +0800

    refactor: rename

--- src/BirdMessenger/HttpClientExtension.cs
+++ src/BirdMessenger/HttpClientExtension.cs
@@ -17,60 +17,60 @@ public static class HttpClientExtension
     /// <summary>
     /// create a url for file upload
     /// </summary>
-    /// <param name="tusCreateRequestOption"></param>
+    /// <param name="reqOption"></param>
     /// <param name="ct"></param>
     /// <returns></returns>
-    public static async Task<TusCreateResponse> TusCreateAsync(this HttpClient httpClient,TusCreateRequestOption tusCreateRequestOption,
+    public static async Task<TusCreateResponse> TusCreateAsync(this HttpClient httpClient,TusCreateRequestOption reqOption,
         CancellationToken ct = default)
     {
-        if (tusCreateRequestOption is null)
+        if (reqOption is null)
         {
             throw new ArgumentException("tusCreateRequestOption is null");
         }
 
-        if (tusCreateRequestOption.Endpoint is null)
+        if (reqOption.Endpoint is null)
         {
             throw new ArgumentException("Endpoint is null");
         }
-        tusCreateRequestOption.ValidateHttpHeaders();
+        reqOption.ValidateHttpHeaders();
 
-        var endpoint = tusCreateRequestOption.Endpoint;
+        var endpoint = reqOption.Endpoint;
         
-        if (tusCreateRequestOption.IsUploadDeferLength && tusCreateRequestOption.UploadLength > 0)
+        if (reqOption.IsUploadDeferLength && reqOption.UploadLength > 0)
         {
-            throw new InvalidDataException("IsUploadDeferLength can not set true if UploadLength is greater than zero");
+            throw new ArgumentException($"IsUploadDeferLength:[{reqOption.IsUploadDeferLength}] can not set true if UploadLength:[{reqOption.UploadLength}] is greater than zero");
         }
         var httpReqMsg = new HttpRequestMessage(HttpMethod.Post, endpoint);
         
         httpReqMsg.Headers.Add(TusHeaders.TusResumable,TusVersion.V1_0_0.GetEnumDescription());
         
-        if (tusCreateRequestOption.UploadLength >= 0)
+        if (reqOption.UploadLength >= 0)
         {
-            httpReqMsg.Headers.Add(TusHeaders.UploadLength, tusCreateRequestOption.UploadLength.ToString());
+            httpReqMsg.Headers.Add(TusHeaders.UploadLength, reqOption.UploadLength.ToString());
         }
 
-        string uploadMetadata = tusCreateRequestOption.Metadata?.Serialize();
+        string uploadMetadata = reqOption.Metadata?.Serialize();
         if (!string.IsNullOrWhiteSpace(uploadMetadata))
         {
             httpReqMsg.Headers.Add(TusHeaders.UploadMetadata, uploadMetadata);
         }
         
-        if (tusCreateRequestOption.HttpHeaders is not null && tusCreateRequestOption.HttpHeaders.Any())
+        if (reqOption.HttpHeaders is not null && reqOption.HttpHeaders.Any())
         {
-            foreach (var key in tusCreateRequestOption.HttpHeaders.Keys)
+            foreach (var key in reqOption.HttpHeaders.Keys)
             {
-                httpReqMsg.Headers.Add(key,tusCreateRequestOption.HttpHeaders[key]);
+                httpReqMsg.Headers.Add(key,reqOption.HttpHeaders[key]);
             }
         }
 
         
-        if (tusCreateRequestOption.OnPreSendRequestAsync is not null)
+        if (reqOption.OnPreSendRequestAsync is not null)
         {
             PreSendRequestEvent preSendRequestEvent = new PreSendRequestEvent
             {
                 HttpRequestMsg = httpReqMsg
             };
-            await tusCreateRequestOption.OnPreSendRequestAsync(preSendRequestEvent);
+            await reqOption.OnPreSendRequestAsync(preSendRequestEvent);
         }
         
         var response = await httpClient.SendAsync(httpReqMsg, ct);
